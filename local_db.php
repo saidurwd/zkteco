118052c897278178ccd14013fd60ba360bcc2874
Saidur Rahman <saidurwd@gmail.com>
2023-07-24 11:22:46 +0600

    json query fix - in and out

--- local_db.php
+++ local_db.php
@@ -10,15 +10,6 @@ if (!file_exists($database_path)) {
 	die("Access database file not found !");
 }
 
-//create a new PDO object
-//$db = new PDO("odbc:DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=realpath($database_path); Uid=$db_username; Pwd=$db_password;");
-
-//$db = new PDO("odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=" . $database_path . ";Uid=; Pwd=;");
-
-//$db = new PDO("odbc:DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=$database_path; Uid=$db_username; Pwd=$db_password;");
-
-//echo $database_path;
-
 try {
 	return $db = new PDO("odbc:DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=$database_path; Uid=; Pwd=;");
 } catch (PDOException $db) {
--- user_attendance.php
+++ user_attendance.php
@@ -1,18 +1,18 @@
+<?php
+    // set local date time zone
+    date_default_timezone_set("Asia/Dhaka");
+    require 'local_db.php';
+    //$mydate = date('Ymd');
+    //$sql  = "SELECT u.Badgenumber as ID, c.CHECKTIME as date_time FROM USERINFO u, CHECKINOUT c WHERE u.USERID = c.USERID";
+    $sql = "SELECT U.Badgenumber AS 'user_id', Format(C.CHECKTIME, 'dd-mm-yyyy') AS 'att_date', Min(Format(C.CHECKTIME, 'hh:nn AM/PM')) AS 'check_in', Max(Format(C.CHECKTIME, 'hh:nn AM/PM')) AS 'check_out'
+            FROM USERINFO U INNER JOIN CHECKINOUT C ON U.USERID = C.USERID 
+            GROUP BY Format(C.CHECKTIME, 'dd-mm-yyyy'), U.Badgenumber";
 
-<?php 
-	// set local date time zone
-	date_default_timezone_set("Asia/Dhaka");
-	require 'local_db.php';
-	//$mydate = date('Ymd');
-	$sql  = "SELECT u.Badgenumber as ID,c.CHECKTIME as date_time FROM USERINFO u, CHECKINOUT c  WHERE u.USERID = c.USERID";
+    $result = $db->query($sql);
+    //print_r($result);
+    $response = array();
 
-	$result = $db->query($sql);
-	//print_r($result);
-	$response = array();
-	   
-	while($row = $result->fetch()){
-			$response[] = $row;
-		}
-	echo json_encode($response);
-
-?>
+    while ($row = $result->fetch()) {
+        $response[] = $row;
+    }
+    echo json_encode($response);
